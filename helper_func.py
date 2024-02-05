6e554b72cba262ff01309e468633e23cb86a19df
AJNAGAA <101543053+AJNAGAA@users.noreply.github.com>
2024-02-05 19:49:52 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -16,7 +16,7 @@ async def is_subscribed(filter, client, update):
     if user_id in ADMINS:
         return True
     try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL or FORCE_SUB_CHANNEL2, user_id = user_id)
     except UserNotParticipant:
         return False
 
